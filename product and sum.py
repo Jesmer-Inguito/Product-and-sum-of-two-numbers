6c7b77dd9c6a3652054dbd237f575fb5af855441
unknown <you@example.com>
2024-01-23 21:48:10 +0800

    if-else statement with results

--- product and sum.py
+++ product and sum.py
@@ -7,9 +7,10 @@ num_1 = int(input('Enter 1st number: '))
 # 2nd number
 num_2 = int(input('Enter 2nd number: '))
 # Check if the product is <= 1000
-
+if num_1 * num_2 <= 1000:
     # print result
-
+    print(num_1 * num_2)
 # else
-
+else:
     #print result
+    print(num_1 + num_2)
