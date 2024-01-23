705eb615670bd74d44eaf3175d4f4bc9af984187
unknown <you@example.com>
2024-01-23 21:45:39 +0800

    input done

--- product and sum.py
+++ product and sum.py
@@ -1,7 +1,7 @@
 # pseudocode
 
 # Ask user to enter two numbers
-print('Please enter teo numbers')
+print('Please enter two numbers')
 # 1st number
 num_1 = int(input('Enter 1st number: '))
 # 2nd number
