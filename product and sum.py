133cb69bcc82395378fa91d19b03f258d626e110
unknown <you@example.com>
2024-01-23 21:44:47 +0800

    input done

--- product and sum.py
+++ product and sum.py
@@ -1,11 +1,11 @@
 # pseudocode
 
 # Ask user to enter two numbers
-
+print('Please enter teo numbers')
 # 1st number
-
+num_1 = int(input('Enter 1st number: '))
 # 2nd number
-
+num_2 = int(input('Enter 2nd number: '))
 # Check if the product is <= 1000
 
     # print result
